9f44391dad8848620619afa6c1bcaf943cfcec83
DeSantisDomiziano <desantis.domiziano@gmail.com>
2023-03-23 11:47:06 +0100

    snack1

---
+++ main.js
@@ -0,0 +1,82 @@
+/* 
+Crea un array composto da 10 automobili.
+Ogni oggetto automobile avrà le seguenti proprietà: marca, modello e alimentazione (benzina, diesel, gpl, elettrico, metano).
+
+Dividi le automobili in 3 array separati: nel primo array solo le auto a benzina, nel secondo solo le auto a diesel, nel terzo il resto delle auto.
+ 
+Infine stampa separatamente i 3 array.
+*/
+
+const auto = [
+    {
+        marca: 'mercedes',
+        modello: 'classe A',
+        alimentazione: 'benzina',
+    },
+    {
+        marca: 'mercedes',
+        modello: 'CLA',
+        alimentazione: 'diesel',
+    },
+    {
+        marca: 'mercedes',
+        modello: 'EQS',
+        alimentazione: 'elettrica',
+    },
+    {
+        marca: 'mercedes',
+        modello: 'EQE',
+        alimentazione: 'elettrica',
+    },
+    {
+        marca: 'mercedes',
+        modello: 'classe C',
+        alimentazione: 'diesel',
+    },
+    {
+        marca: 'mercedes',
+        modello: 'classe E',
+        alimentazione: 'benzina',
+    },
+    {
+        marca: 'mercedes',
+        modello: 'EQA',
+        alimentazione: 'elettrica',
+    },
+    {
+        marca: 'mercedes',
+        modello: 'CLS',
+        alimentazione: 'benzina',
+    },
+    {
+        marca: 'mercedes',
+        modello: 'AMG GT',
+        alimentazione: 'benzina',
+    },
+    {
+        marca: 'mercedes',
+        modello: 'SL Roadster',
+        alimentazione: 'benzina',
+    },
+]
+
+const alimentazione_benzina = auto.filter((car) => {
+    if (car.alimentazione == 'benzina') {
+        return true
+    }
+})
+console.log(alimentazione_benzina);
+
+const alimentazione_diesel = auto.filter((car) => {
+    if (car.alimentazione == 'diesel') {
+        return true
+    }
+})
+console.log(alimentazione_diesel);
+
+const alimentazione_elettrica = auto.filter((car) => {
+    if (car.alimentazione == 'elettrica') {
+        return true
+    }
+})
+console.log(alimentazione_elettrica);
